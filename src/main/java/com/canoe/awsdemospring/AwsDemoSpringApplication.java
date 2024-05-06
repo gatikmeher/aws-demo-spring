3ad4e5c4ea4368ee41dd247ee68926998ac104b7
Gati K Meher <gatimeher@gmail.com>
2024-05-06 19:18:15 +0530

    initial commit

---
+++ src/main/java/com/canoe/awsdemospring/AwsDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.canoe.awsdemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDemoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canoe/awsdemospring/AwsDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canoe.awsdemospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
