a0da764cb740552697f575b4dfef17fb6af5c7a1
Gati K Meher <gatimeher@gmail.com>
2024-05-08 21:25:45 +0530

    Updated

--- src/main/java/com/canoe/awsdemospring/controller/DemoController.java
+++ src/main/java/com/canoe/awsdemospring/controller/DemoController.java
@@ -4,14 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/v1")
 public class DemoController {
 
 
-    @RequestMapping("/v1")
+    @RequestMapping("/demo")
     public ResponseEntity<Object> getDemo() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @RequestMapping("/v1")
+    public ResponseEntity<Object> getUser() {
+        Map<String, String> users = Map.of("name", "John", "age", "25");
+        return ResponseEntity.ok(users);
     }
 
 }
